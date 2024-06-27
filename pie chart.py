2e4f759739541b73b0393400102fe21c67d221bc
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-06-26 21:03:44 -0400

    Update pie chart.py

--- pie chart.py
+++ pie chart.py
@@ -1,22 +1,9 @@
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
-data = pd.read_csv('employee.csv')
+y = [35, 25, 25, 15]
+x = ["Apples", "Bananas", "Cherries", "Dates"]
 
-department_counts = data['Department'].value_counts()
-
-# Extract the department names as categories
-categories = department_counts.index.tolist()
-
-# Extract the count of employees as proportions
-proportions = department_counts.values.tolist()
-
-# Create a pie chart with the proportions and labels
-plt.pie(proportions, labels=categories, autopct='%1.1f%%')
-
-# Set the title of the chart
-plt.title('Pie Chart of Departments')
-
-# Display the chart
-plt.show()
+plt.pie(y, labels = x)
+plt.legend()
+plt.show() 

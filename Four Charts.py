2a142239b8e10dfe9812d2d138b335c10b54750b
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-06-26 21:01:35 -0400

    Add files via upload

---
+++ Four Charts.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Bar Chart
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 4, 1)
+plt.bar(x1, y1)
+plt.title('Bar Chart')
+
+# Line Chart
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 4, 2)
+plt.plot(x2, y2)
+plt.title('Line Chart')
+
+# Scatter Plot
+x3 = np.array([0, 1, 2, 3])
+y3 = np.array([5, 15, 25, 35])
+
+plt.subplot(1, 4, 3)
+plt.scatter(x3, y3)
+plt.title('Scatter Plot')
+
+# Pie Chart
+x4 = np.array(["Apples", "Bananas", "Cherries", "Dates"])
+y4 = np.array([35, 25, 25, 15])
+
+plt.subplot(1, 4, 4)
+plt.pie(sizes, labels=labels)
+plt.legend()
+plt.title('Pie Chart')
+
+plt.tight_layout()  # Adjust layout to prevent overlapping
+plt.show()

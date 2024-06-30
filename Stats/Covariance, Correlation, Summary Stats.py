ad9d579a664fd1f01252117ecc6d0eb2c5ed6c12
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-06-29 22:43:17 -0400

    Update and rename Summary.py to Covariance, Correlation, Summary Stats.py

---
+++ Stats/Covariance, Correlation, Summary Stats.py
@@ -0,0 +1,15 @@
+from scipy.stats import skew, skewtest
+import pandas as pd
+from sklearn.datasets import load_iris
+
+# Load the iris dataset
+iris = load_iris()
+iris_dataframe = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+
+
+print(iris_dataframe.corr()) #Correlation
+print(iris_dataframe.cov() )#Covariance
+print(iris_dataframe.describe()) #Summary Stats
+
+

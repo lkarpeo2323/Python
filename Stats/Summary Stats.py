331867d8dfd95d30f70d26ec27fd112c7eaf2c6f
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-14 10:48:16 -0400

    Update Summary Stats.py

--- Stats/Summary Stats.py
+++ Stats/Summary Stats.py
@@ -4,10 +4,12 @@ from scipy import stats
 # Create sample data and load it into a DataFrame
 data = pd.DataFrame({
     'x': [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6],
+    'y': [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6],
+    'z': [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
 })
 
 # Get descriptive statistics for 'x'
-leo = data['x'].describe()
+leo = data.describe()
 
 # Print the descriptive statistics
 print(leo)

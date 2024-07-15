9ade53e279770462bb1c40943bffa649eced345f
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-15 10:19:09 -0400

    Update Scatter plot.py

--- Charts/Scatter plot.py
+++ Charts/Scatter plot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-classes = [1,2,0,1,2,0,0,1,2,0] #Clases represent different colors
+classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]  #Clases represent different colors
 
 plt.scatter(x, y, c=classes)
-plt.show()
+plt.show() 

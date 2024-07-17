51e976596e4359fb0c984b6deb4dcf496cb5b85f
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-17 11:38:39 -0400

    Update web scraping.py

--- Charts/web scraping.py
+++ Charts/web scraping.py
@@ -3,3 +3,6 @@ import metadata_parser
 url = 'https://portfolio-karpel-website.netlify.app/academics'
 page = metadata_parser.MetadataParser(url)
 print(page.metadata)
+
+
+print(page.get_metadatas('title')) #get the title

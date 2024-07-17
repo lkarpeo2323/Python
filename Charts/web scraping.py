2e1686f558d0218126bd4856e85be76bb12e921c
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-17 14:24:10 -0400

    Delete Charts/web scraping.py

--- Charts/web scraping.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://essentialdata.com/technical-writers/automotive-technical-writer/'
-
-soup = BeautifulSoup(requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).content, 'html.parser')
-print('Title:', soup.title.string.strip() if soup.title else 'No title found')
-print('Meta Description:', next((meta.attrs['content'] for meta in soup.find_all('meta') if 'name' in meta.attrs and meta.attrs['name'].lower() == 'description'), 'No meta description found'))
-

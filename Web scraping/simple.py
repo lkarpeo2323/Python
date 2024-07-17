4ff725bb3f2c21c832fd2bd611c2431c20936fc7
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-17 17:14:25 -0400

    Update simple.py

--- Web scraping/simple.py
+++ Web scraping/simple.py
@@ -2,4 +2,8 @@ import metadata_parser
 
 url = 'https://portfolio-karpel-website.netlify.app/index.html'
 page = metadata_parser.MetadataParser(url)
-print(page.metadata)
+
+
+print('Title:' ,page.get_metadatas('title'))
+
+print('Meta Description:', page.get_metadatas('description'))

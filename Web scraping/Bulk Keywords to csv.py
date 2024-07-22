973a72dfc6f8524a2840cadcdf3b29f6793dd792
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-22 11:08:03 -0400

    Update Bulk Keywords to csv.py

--- Web scraping/Bulk Keywords to csv.py
+++ Web scraping/Bulk Keywords to csv.py
@@ -9,7 +9,7 @@ import pandas as pd
 # Ensure the stopwords are downloaded
 nltk.download('stopwords')
 
-def get_top_words(url, top_n=10):
+def get_top_words(url, top_n=5):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }

4ffe3c5ed731a1af8fa5dddc8798ef8280faac52
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-22 15:57:50 -0400

    Update and rename Meta and Title.py to Meta and HTML Tags.py

--- Web scraping/Meta and HTML Tags.py
+++ Web scraping/Meta and HTML Tags.py
@@ -1,8 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://essentialdata.com/technical-writers/automotive-technical-writer/'
+url = 'https://portfolio-karpel-website.netlify.app/index.html'
 
 soup = BeautifulSoup(requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).content, 'html.parser')
-print('Title:', soup.title.string.strip() if soup.title else 'No title found')
+
+#Title
+print('Title:', soup.title.string.strip() if soup.title else 'No title found') 
+
+#Meta Description
 print('Meta Description:', next((meta.attrs['content'] for meta in soup.find_all('meta') if 'name' in meta.attrs and meta.attrs['name'].lower() == 'description'), 'No meta description found'))
+
+#h1
+print(soup.h1)
+
+#h2
+print(soup.h2)
+
+#h3
+print(soup.h3)

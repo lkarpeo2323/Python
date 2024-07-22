2972a21b7f8c6609feff2d183d221e63f999fc3b
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-07-22 15:58:07 -0400

    Delete Web scraping/Individual HTML Tags.py

--- Web scraping/Individual HTML Tags.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen('https://portfolio-karpel-website.netlify.app/index.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-
-print(bs.h1)
-print(bs.h2)
-print(bs.title)

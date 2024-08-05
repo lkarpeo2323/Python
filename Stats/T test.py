ed27b7c6cfeec77107b02b499ed85fdc2ab5ffba
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-05 17:38:28 -0400

    Update T test.py

--- Stats/T test.py
+++ Stats/T test.py
@@ -1,21 +1,14 @@
-
 import numpy as np
 from scipy import stats
 import pandas as pd
 
+data = pd.read_excel('excel.xlsx')
 
-
-data = pd.read_excel('t test.xlsx')
-
-
-under = data['Clicks']
-over = data['Clicks over']
-
-# Example data
-
+clicks = data['Clicks']
+position = data['Position']
 
 # Perform t-test
-t_stat, p_value = stats.ttest_ind(under, over, equal_var=False)
+t_stat, p_value = stats.ttest_ind(clicks, position, equal_var=False)
 
 # Print results
 print(f"T-Statistic: {t_stat:.3f}")
@@ -23,6 +16,6 @@ print(f"P-Value: {p_value:.3f}")
 
 # Interpret the results
 if p_value < 0.05:
-    print("There is a significant difference between the over and under.")
+    print("There is a significant difference between the clicks and position.")
 else:
-    print("There is no significant difference between the clicks for Position 1 and other positions.")
+    print("There is no significant difference between the clicks and position.")

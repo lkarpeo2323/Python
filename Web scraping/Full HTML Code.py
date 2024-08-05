673a6dac41ebd3bb695bede2bf27b41384fa6047
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-04 22:27:52 -0400

    Delete Web scraping/Full HTML Code.py

--- Web scraping/Full HTML Code.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-
-url = 'https://essentialdata.com/service/acceptable-use-policy-writing/'
-response = urlopen(url)
-html = response.read().decode('utf-8')
-
-print(html)

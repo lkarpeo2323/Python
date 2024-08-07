24ebb50e66cbd7a86becf120d2f6df5527c1df3c
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-07 11:57:34 -0400

    Update Line Chart.py

--- Charts/Line Chart.py
+++ Charts/Line Chart.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
-%matplotlib inline
+import pandas as pd
 
-values = [1,5,8,9,2,0,3,10,4,7]
-values2 = [3,8,9,2,1,2,4,7,6,6]
 
-plt.plot(range(1,11), values, 'o--')
-plt.plot(range(1,11), values2, 'v:')
+data = pd.read_excel('Word Counts.xlsx')
 
+over = data['Over']
+under = data['Under']
+
+
+plt.plot(over, label='Pages with Over 1558 Word Count') 
+plt.plot(under, label='Pages with Under 1558 Word Count')
+plt.ylabel('Clicks') #label on the y-axis
+plt.legend()
 plt.show()

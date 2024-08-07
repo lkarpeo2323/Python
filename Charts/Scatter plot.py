c238afd8be6785713722621ac2272ab09af0dc13
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-07 11:13:37 -0400

    Update Scatter plot.py

--- Charts/Scatter plot.py
+++ Charts/Scatter plot.py
@@ -1,8 +1,12 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 
-x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]  #Classes represent different colors
 
-plt.scatter(x, y, c=classes)
+data = pd.read_excel('Word Counts.xlsx')
+
+x = data['Word Count']
+y = data['Clicks']
+
+
+plt.scatter(x,y)
 plt.show() 

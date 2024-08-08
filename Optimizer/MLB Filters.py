b46fa6f8a83693bb70f91b12ee5cf420b9cb3b6a
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-08 12:29:54 -0400

    Update MLB Filters.py

--- Optimizer/MLB Filters.py
+++ Optimizer/MLB Filters.py
@@ -11,28 +11,29 @@ optimizer.load_players_from_csv("fantasy.csv")
 # Access the player pool
 pool = optimizer.player_pool
 
-#EXclude a Player
+#Exclude a Player
 optimizer.player_pool.remove_player('Adrian Del Castillo')
-optimizer.player_pool.remove_player('Ben Rice')
 optimizer.player_pool.remove_player('Jordan Montgomery')
-
+optimizer.player_pool.remove_player('Brandon Marsh')
 
 #Lock a Player
-optimizer.player_pool.lock_player('Aaron Judge')
+optimizer.player_pool.lock_player('Randal Grichuk')
+optimizer.player_pool.lock_player('Josh Lowe')
+optimizer.player_pool.lock_player('Brandon Lowe')
+optimizer.player_pool.lock_player('Bryan Woo')
+optimizer.player_pool.lock_player('Trea Turner')
 
 
-
-#Exclude a Team
-optimizer.player_pool.exclude_teams(['MIA'])
-optimizer.player_pool.exclude_teams(['LAA'])
+#Exclude Teams
+optimizer.player_pool.exclude_teams(['NYY', 'LAA'])
 
 
 # Set max exposure for all players
 for player in pool.all_players:
-    player.max_exposure = 0.6
+    player.max_exposure = .6
 
 # Optimize lineups with the specified exposure limits
-lineup_generator = optimizer.optimize(n=10)
+lineup_generator = optimizer.optimize(n=20)
 
 # Print each optimized lineup
 for lineup in lineup_generator:

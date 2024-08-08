1a05eccdeff06bb09a347a18c775fba2f73b6923
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-08 11:50:31 -0400

    Update MLB Filters.py

--- Optimizer/MLB Filters.py
+++ Optimizer/MLB Filters.py
@@ -12,14 +12,24 @@ optimizer.load_players_from_csv("fantasy.csv")
 pool = optimizer.player_pool
 
 #EXclude a Player
-optimizer.player_pool.remove_player('Bligh Madris')
+optimizer.player_pool.remove_player('Adrian Del Castillo')
+optimizer.player_pool.remove_player('Ben Rice')
+optimizer.player_pool.remove_player('Jordan Montgomery')
+
+
+#Lock a Player
+optimizer.player_pool.lock_player('Aaron Judge')
+
+
 
 #Exclude a Team
-optimizer.player_pool.exclude_teams(['BAL'])
+optimizer.player_pool.exclude_teams(['MIA'])
+optimizer.player_pool.exclude_teams(['LAA'])
+
 
 # Set max exposure for all players
 for player in pool.all_players:
-    player.max_exposure = 0.6
+    player.max_exposure = 0.9
 
 # Optimize lineups with the specified exposure limits
 lineup_generator = optimizer.optimize(n=10)

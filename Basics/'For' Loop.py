580e872495d7f00bd9d1de69ba37ff7c0b0e3f19
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-20 19:22:16 -0400

    Update 'For' Loop.py

--- Basics/'For' Loop.py
+++ Basics/'For' Loop.py
@@ -7,10 +7,28 @@ for i in text:
 
 
 
-#More advanced 
+#Level 2
 
 text = 'hello world'
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for i in text:
     index = alphabet.find(i)
     print(i, 'is', index)
+
+
+#Level 3
+
+text = 'hello world'
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+mom = ' '
+for char in text:
+    if char == ' ':
+        mom += char
+    else:
+        index = alphabet.find(char)
+        print(char, 'is', index) # There is no space included in the index
+
+
+
+
+

3d99a38b8e812524e7cbc4fbc8b158d95525afeb
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-11-26 17:57:19 -0500

    Update T test.py

--- Stats/T test.py
+++ Stats/T test.py
@@ -1,21 +1,6 @@
-import numpy as np
-from scipy import stats
-import pandas as pd
-
-data = pd.read_excel('excel.xlsx')
-
-clicks = data['Clicks']
-position = data['Position']
-
-# Perform t-test
-t_stat, p_value = stats.ttest_ind(clicks, position, equal_var=False)
-
-# Print results
-print(f"T-Statistic: {t_stat:.3f}")
-print(f"P-Value: {p_value:.3f}")
-
-# Interpret the results
-if p_value < 0.05:
-    print("There is a significant difference between the clicks and position.")
-else:
-    print("There is no significant difference between the clicks and position.")
+Student, ID,	University,	Weight,
+1	Hofstra	187
+2	Hofstra	135
+3	Hofstra	145
+4	Baruch	107
+5	Baruch	120
